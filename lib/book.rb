245b44b0da2db47fdff9260ad20e4856a59f00e1
Stephen Arancio <sjarancio@gmail.com>
2021-08-11 11:41:21 -0400

    commit tree 2b0a58bb9b204ca9ea681dcb292396898ac6fb0a

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,13 @@
-
+class Book
+    attr_accessor :title
+    def initialize(title)
+        @title = title
+    end
+    attr_accessor :author
+    attr_accessor :genre
+    attr_accessor :page_count
+    def turn_page 
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
